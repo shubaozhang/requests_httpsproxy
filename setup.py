7cf231c243c4170a36a4999bb2fbdf3e2ba857af
Phus Lu <phuslu@hotmail.com>
2017-02-24 20:16:40 +0800

    bump version to 1.0.2
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

--- setup.py
+++ setup.py
@@ -4,15 +4,29 @@ from setuptools import setup, find_packages
 
 setup(
     name='requests_httpsproxy',
-    version='1.0.1',
+    version='1.0.2',
     description='allow http/https requests through https proxy',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    keywords='requests https-proxy',
+    url='https://github.com/phuslu/requests_httpsproxy',
+    license='MIT',
     author='Phus Lu',
     author_email='phuslu@hotmail.com',
-    url='https://github.com/phuslu/requests_httpsproxy',
     packages=find_packages(),
-    zip_safe=True,
-    include_package_data=True,
-    license='MIT',
+    install_requires=[
+        'requests>=2.13.0',
+        'pyOpenSSL>=0.11',
+        'tlslite-ng'
+    ],
     platforms='any',
-    install_requires=['requests>=2.13.0', 'pyOpenSSL>=0.11', 'tlslite-ng'],
+    include_package_data=True,
+    zip_safe=True,
 )
