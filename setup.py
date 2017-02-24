1e5fcecae702877f53985216086cc647bf8e428d
Phus Lu <phuslu@hotmail.com>
2017-02-24 12:38:09 +0800

    add setup.py
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='requests_httpsproxy',
+    version='1.0',
+    description='allow http/https requests through https proxy',
+    author='Phus Lu',
+    author_email='phuslu@hotmail.com',
+    url='https://github.com/phuslu/requests_httpsproxy',
+    packages=find_packages(),
+    zip_safe=True,
+    include_package_data=True,
+    license='MIT',
+    platforms='any',
+    install_requires=['requests>=2.13.0', 'pyOpenSSL>=0.11', 'tlslite-ng'],
+)
